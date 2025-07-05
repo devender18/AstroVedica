dd3453ac2759b0f4e8128a5f45dab4d2ebfff1f3
devender18 <devendersingh2k@gmail.com>
2025-07-05 09:03:27 +0530

    landing page UI

---
+++ frontend/src/screen/StitchDesign/StitchDesign.jsx
@@ -0,0 +1,215 @@
+import React from "react";
+
+export const StitchDesign = () => {
+  return (
+    <div
+      className="flex flex-col items-start relative bg-white"
+      data-model-id="1:2"
+    >
+      <div className="flex flex-col min-h-[800px] items-start relative self-stretch w-full flex-[0_0_auto] bg-[#111416]">
+        <div className="flex flex-col items-start relative self-stretch w-full flex-[0_0_auto]">
+          <div className="items-center justify-between px-10 py-3 flex-[0_0_auto] border-b [border-bottom-style:solid] border-[#e5e8ea] flex relative self-stretch w-full">
+            <div className="inline-flex items-center gap-4 relative flex-[0_0_auto]">
+              <div className="inline-flex flex-col items-start relative flex-[0_0_auto]">
+                <div className="w-4 bg-[url(https://c.animaapp.com/mcpndo5htHmUgt/img/vector---0.svg)] bg-[100%_100%] relative flex-1 grow" />
+              </div>
+
+              <div className="inline-flex flex-col items-start relative flex-[0_0_auto]">
+                <div className="relative self-stretch mt-[-1.00px] [font-family:'Space_Grotesk',Helvetica] font-bold text-white text-lg tracking-[0] leading-[23px] whitespace-nowrap">
+                  vedicAstro
+                </div>
+              </div>
+            </div>
+
+            <div className="flex items-start justify-end gap-8 relative flex-1 grow">
+              <div className="inline-flex h-10 items-center gap-9 relative flex-[0_0_auto]">
+                <div className="inline-flex flex-col items-start relative flex-[0_0_auto]">
+                  <div className="relative self-stretch mt-[-1.00px] [font-family:'Space_Grotesk',Helvetica] font-medium text-white text-sm tracking-[0] leading-[21px] whitespace-nowrap">
+                    Daily Horoscope
+                  </div>
+                </div>
+
+                <div className="inline-flex flex-col items-start relative flex-[0_0_auto]">
+                  <div className="relative self-stretch mt-[-1.00px] [font-family:'Space_Grotesk',Helvetica] font-medium text-white text-sm tracking-[0] leading-[21px] whitespace-nowrap">
+                    Kundli
+                  </div>
+                </div>
+
+                <div className="inline-flex flex-col items-start relative flex-[0_0_auto]">
+                  <div className="relative self-stretch mt-[-1.00px] [font-family:'Space_Grotesk',Helvetica] font-medium text-white text-sm tracking-[0] leading-[21px] whitespace-nowrap">
+                    Compatibility
+                  </div>
+                </div>
+
+                <div className="inline-flex flex-col items-start relative flex-[0_0_auto]">
+                  <div className="relative self-stretch mt-[-1.00px] [font-family:'Space_Grotesk',Helvetica] font-medium text-white text-sm tracking-[0] leading-[21px] whitespace-nowrap">
+                    Tarot
+                  </div>
+                </div>
+
+                <div className="inline-flex flex-col items-start relative flex-[0_0_auto]">
+                  <div className="relative self-stretch mt-[-1.00px] [font-family:'Space_Grotesk',Helvetica] font-medium text-white text-sm tracking-[0] leading-[21px] whitespace-nowrap">
+                    Numerology
+                  </div>
+                </div>
+
+                <div className="inline-flex flex-col items-start relative flex-[0_0_auto]">
+                  <div className="relative self-stretch mt-[-1.00px] [font-family:'Space_Grotesk',Helvetica] font-medium text-white text-sm tracking-[0] leading-[21px] whitespace-nowrap">
+                    Ask Astrologer
+                  </div>
+                </div>
+              </div>
+
+              <div className="inline-flex items-start gap-2 relative flex-[0_0_auto]">
+                <div className="inline-flex min-w-[84px] flex-[0_0_auto] bg-[#1977e5] max-w-[480px] h-10 items-center justify-center px-4 py-0 relative rounded-lg overflow-hidden">
+                  <div className="inline-flex flex-col items-center relative flex-[0_0_auto]">
+                    <div className="relative self-stretch mt-[-1.00px] [font-family:'Space_Grotesk',Helvetica] font-bold text-white text-sm text-center tracking-[0] leading-[21px] whitespace-nowrap">
+                      Sign up
+                    </div>
+                  </div>
+                </div>
+
+                <div className="flex w-[84px] bg-[#283038] max-w-[480px] h-10 items-center justify-center px-4 py-0 relative rounded-lg overflow-hidden">
+                  <div className="inline-flex flex-col items-center relative flex-[0_0_auto]">
+                    <div className="relative self-stretch mt-[-1.00px] [font-family:'Space_Grotesk',Helvetica] font-bold text-white text-sm text-center tracking-[0] leading-[21px] whitespace-nowrap">
+                      Login
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="items-start justify-center px-40 py-5 flex-1 grow flex relative self-stretch w-full">
+            <div className="flex flex-col max-w-[960px] items-start relative flex-1 grow">
+              <div className="flex flex-col items-start relative self-stretch w-full flex-[0_0_auto]">
+                <div className="flex flex-col items-start p-4 self-stretch w-full relative flex-1 grow">
+                  <div className="h-[480px] bg-[linear-gradient(90deg,rgba(0,0,0,0.1)_0%,rgba(0,0,0,0.4)_100%),url(https://c.animaapp.com/mcpndo5htHmUgt/img/depth-6--frame-0.png)_50%_50%_/_cover] relative self-stretch w-full rounded-lg overflow-hidden">
+                    <div className="flex flex-col w-[896px] items-start gap-2 absolute top-[235px] left-48">
+                      <div className="h-[60px] flex flex-col items-center relative self-stretch w-full">
+                        <div className="relative self-stretch mt-[-1.00px] [font-family:'Space_Grotesk',Helvetica] font-bold text-white text-5xl text-center tracking-[-2.00px] leading-[60px]">
+                          Unlock Your Cosmic Blueprint
+                        </div>
+                      </div>
+
+                      <div className="flex-[0_0_auto] flex flex-col items-center relative self-stretch w-full">
+                        <p className="relative self-stretch mt-[-1.00px] [font-family:'Space_Grotesk',Helvetica] font-normal text-white text-base text-center tracking-[0] leading-6">
+                          Discover your unique astrological profile with our
+                          precise horoscope calculator. Enter your birth details
+                          to reveal insights into your personality,
+                          relationships, and life path.
+                        </p>
+                      </div>
+                    </div>
+
+                    <div className="flex flex-col max-w-[480px] w-[480px] h-16 items-start absolute top-[383px] left-[400px]">
+                      <div className="flex items-start relative flex-1 self-stretch w-full grow rounded-lg">
+                        <div className="inline-flex justify-center pl-[15px] pr-0 py-0 flex-[0_0_auto] rounded-[8px_0px_0px_8px] border-l [border-left-style:solid] items-center relative self-stretch bg-[#1c2126] border-t [border-top-style:solid] border-b [border-bottom-style:solid] border-[#3d4754]">
+                          <div className="relative flex-1 grow h-5 bg-[url(https://c.animaapp.com/mcpndo5htHmUgt/img/vector---0-4.svg)] bg-[100%_100%]" />
+                        </div>
+
+                        <div className="flex p-2 flex-1 grow items-center relative self-stretch bg-[#1c2126] border-t [border-top-style:solid] border-b [border-bottom-style:solid] border-[#3d4754]">
+                          <div className="relative w-fit [font-family:'Space_Grotesk',Helvetica] font-normal text-[#9eaab7] text-base tracking-[0] leading-6 whitespace-nowrap">
+                            Enter&nbsp;&nbsp;your name
+                          </div>
+                        </div>
+
+                        <div className="inline-flex justify-center pl-0 pr-[7px] py-0 flex-[0_0_auto] rounded-[0px_8px_8px_0px] border-r [border-right-style:solid] items-center relative self-stretch bg-[#1c2126] border-t [border-top-style:solid] border-b [border-bottom-style:solid] border-[#3d4754]">
+                          <div className="flex min-w-[84px] max-w-[480px] h-12 items-center justify-center px-5 py-0 relative flex-1 grow bg-[#1977e5] rounded-lg overflow-hidden">
+                            <div className="inline-flex flex-col items-center relative flex-[0_0_auto]">
+                              <div className="relative self-stretch mt-[-1.00px] [font-family:'Space_Grotesk',Helvetica] font-bold text-white text-base text-center tracking-[0] leading-6 whitespace-nowrap">
+                                Calculate Horoscope
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div className="inline-flex flex-wrap max-w-[480px] items-end gap-[16px_16px] px-4 py-3 relative flex-[0_0_auto]">
+                <div className="flex flex-col min-w-40 items-start relative flex-1 grow">
+                  <div className="flex items-start relative flex-1 self-stretch w-full grow rounded-lg">
+                    <div className="flex h-14 pl-[15px] pr-2 py-[15px] flex-1 grow rounded-[8px_0px_0px_8px] overflow-hidden border-l [border-left-style:solid] items-center relative bg-[#1c2126] border-t [border-top-style:solid] border-b [border-bottom-style:solid] border-[#3d4754]">
+                      <div className="relative w-fit [font-family:'Space_Grotesk',Helvetica] font-normal text-[#9eaab7] text-base tracking-[0] leading-6 whitespace-nowrap">
+                        Date&nbsp;&nbsp;of Birth
+                      </div>
+                    </div>
+
+                    <div className="inline-flex justify-center pl-0 pr-[15px] py-0 self-stretch flex-[0_0_auto] rounded-[0px_8px_8px_0px] border-r [border-right-style:solid] items-center relative bg-[#1c2126] border-t [border-top-style:solid] border-b [border-bottom-style:solid] border-[#3d4754]">
+                      <div className="bg-[url(https://c.animaapp.com/mcpndo5htHmUgt/img/vector---0-2.svg)] relative flex-1 grow h-6 bg-[100%_100%]" />
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div className="inline-flex flex-wrap max-w-[480px] items-end gap-[16px_16px] px-4 py-3 relative flex-[0_0_auto]">
+                <div className="flex flex-col min-w-40 items-start relative flex-1 grow">
+                  <div className="flex items-start relative flex-1 self-stretch w-full grow rounded-lg">
+                    <div className="flex h-14 pl-[15px] pr-2 py-[15px] flex-1 grow rounded-[8px_0px_0px_8px] overflow-hidden border-l [border-left-style:solid] items-center relative bg-[#1c2126] border-t [border-top-style:solid] border-b [border-bottom-style:solid] border-[#3d4754]">
+                      <div className="relative w-fit [font-family:'Space_Grotesk',Helvetica] font-normal text-[#9eaab7] text-base tracking-[0] leading-6 whitespace-nowrap">
+                        Time&nbsp;&nbsp;of Birth
+                      </div>
+                    </div>
+
+                    <div className="inline-flex justify-center pl-0 pr-[15px] py-0 self-stretch flex-[0_0_auto] rounded-[0px_8px_8px_0px] border-r [border-right-style:solid] items-center relative bg-[#1c2126] border-t [border-top-style:solid] border-b [border-bottom-style:solid] border-[#3d4754]">
+                      <div className="bg-[url(https://c.animaapp.com/mcpndo5htHmUgt/img/vector---0-1.svg)] relative flex-1 grow h-6 bg-[100%_100%]" />
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div className="inline-flex flex-wrap max-w-[480px] items-end gap-[16px_16px] px-4 py-3 relative flex-[0_0_auto]">
+                <div className="flex flex-col min-w-40 items-start relative flex-1 grow">
+                  <div className="flex h-14 items-center p-[15px] relative self-stretch w-full bg-[#1c2126] rounded-lg overflow-hidden border border-solid border-[#3d4754]">
+                    <div className="relative w-fit [font-family:'Space_Grotesk',Helvetica] font-normal text-[#9eaab7] text-base tracking-[0] leading-6 whitespace-nowrap">
+                      Latitude
+                    </div>
+                  </div>
+                </div>
+
+                <div className="flex flex-col min-w-40 items-start relative flex-1 grow">
+                  <div className="flex h-14 items-center p-[15px] relative self-stretch w-full bg-[#1c2126] rounded-lg overflow-hidden border border-solid border-[#3d4754]">
+                    <div className="relative w-fit [font-family:'Space_Grotesk',Helvetica] font-normal text-[#9eaab7] text-base tracking-[0] leading-6 whitespace-nowrap">
+                      Longitude
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div className="inline-flex flex-wrap max-w-[480px] items-end gap-[16px_16px] px-4 py-3 relative flex-[0_0_auto]">
+                <div className="flex flex-col min-w-40 items-start relative flex-1 grow">
+                  <div className="h-14 bg-[#1c2126] border border-solid border-[#3d4754] relative self-stretch w-full rounded-lg overflow-hidden">
+                    <div className="relative w-[448px] h-[57px] -top-px">
+                      <img
+                        className="absolute w-[448px] h-14 top-px left-0"
+                        alt="Vector"
+                        src="https://c.animaapp.com/mcpndo5htHmUgt/img/vector---0-3.svg"
+                      />
+
+                      <div className="absolute top-0 left-0 [font-family:'Space_Grotesk',Helvetica] font-normal text-white text-base tracking-[0] leading-6 whitespace-nowrap">
+                        Timezone
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+
+              <div className="flex items-start justify-center px-4 py-3 self-stretch w-full relative flex-[0_0_auto]">
+                <div className="inline-flex min-w-[84px] flex-[0_0_auto] bg-[#1977e5] max-w-[480px] h-10 items-center justify-center px-4 py-0 relative rounded-lg overflow-hidden">
+                  <div className="inline-flex flex-col items-center relative flex-[0_0_auto]">
+                    <div className="relative self-stretch mt-[-1.00px] [font-family:'Space_Grotesk',Helvetica] font-bold text-white text-sm text-center tracking-[0] leading-[21px] whitespace-nowrap">
+                      Calculate Horoscope
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { screenGraphPlugin } from "@animaapp/vite-plugin-screen-graph";
+import tailwind from "tailwindcss";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  publicDir: "./static",
+  base: "./",
+  css: {
+    postcss: {
+      plugins: [tailwind()],
+    },
+  },
 })
